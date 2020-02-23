770e016af5e5f114a9c02b918a36f358771f57e6
Kamil Tarczewski <kamil.tarczewski@woodpecker.co>
2020-02-23 14:43:45 +0100

    Clear repo and add new .gitignore

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Created by Rave
-
-mvn clean install && \
-rm -fr ~/quakeServer/stats/stats.jar && \
-cp ./target/quakeStats-1.0-SNAPSHOT-jar-with-dependencies.jar ~/quakeServer/stats/stats.jar
